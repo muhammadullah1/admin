14ce61df7a7c1366114c18bfb943d2ac0d473b2e
muhammadullah1 <muhammadullah403@gmail.com>
2024-09-20 01:26:44 +0500

    .

--- src/config/firebaeConfig.ts
+++ src/config/firebaeConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC60AueBk6hPrCIUd2616z2T_EsXX3SHuQ",
-  authDomain: "testing-c78e4.firebaseapp.com",
-  projectId: "testing-c78e4",
-  storageBucket: "testing-c78e4.appspot.com",
-  messagingSenderId: "443049705379",
-  appId: "1:443049705379:web:b39bc485739377f5a0a416",
-  measurementId: "G-NDZ0QVG2X9"
+  apiKey: 
+  authDomain:
+  projectId:
+  storageBucket:
+  messagingSenderId:
+  appId:
+  measurementId:
 };
 
 const app = initializeApp(firebaseConfig);
